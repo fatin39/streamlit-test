3f6012d2c0f458e2b6b0bd0bd1df8cecaa3d7b22
fatin39 <nurfatinaqilah23@gmail.com>
2023-11-26 19:19:49 +0800

    Add dots

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plost
 
-# hello world ! hi :) how r u?
+# hello world ! hi :) how r u? ......
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
